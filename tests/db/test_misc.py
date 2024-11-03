0b47fb1a93158762629dddd05176bdef8353964f
Jackylee <jieli.work@qq.com>
2024-11-04 00:04:56 +0800

    ✅ update misc db test

--- tests/db/test_misc.py
+++ tests/db/test_misc.py
@@ -1,6 +1,7 @@
-from sqlite3 import connect
 from db.misc import BizContactHeadImg, Misc
+import uuid
 import pytest
+from sqlalchemy import null
 
 wxid = "wxid_h8cex1v6segm21"
 
@@ -14,21 +15,28 @@ def misc() -> Misc:
 
 
 def test_singleton(misc: Misc):
+    """测试单例"""
     misc2 = Misc(wxid)
     assert misc == misc2
 
 
-def test_query(misc: Misc):
+def test_query_all(misc: Misc):
+    """测试查询全部"""
     res = misc.query_all(BizContactHeadImg)
     print(res)
     assert res is not None
 
 
 def test_insert(misc: Misc):
-    misc.insert_all([BizContactHeadImg(usrName="xxx", createTime=1)])
+    """测试插入"""
+    usrName = str(uuid.uuid4())
+    misc.insert_all(
+        [BizContactHeadImg(usrName=usrName, createTime=1, smallHeadImgBuf=null())]
+    )
 
 
 def test_update(misc: Misc):
+    """测试更新"""
     biz = (
         misc.session.query(BizContactHeadImg)
         .filter(BizContactHeadImg.usrName == "2")
@@ -44,3 +52,8 @@ def test_update_db_by_table(misc: Misc):
 
 def test_update_db(misc: Misc):
     misc.update_db_from_cache()
+
+
+def test_query_avatar(misc: Misc):
+    avatar = misc.get_avatar_buffer("2")
+    print(avatar)
