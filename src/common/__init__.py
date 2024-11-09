a2ca8e8fa9ea1c56523f6d0a25c9299562e15ffa
Jackylee <jieli.work@qq.com>
2024-11-10 00:28:00 +0800

    🔥 remove bunch of code

--- src/common/__init__.py
+++
@@ -1,6 +0,0 @@
-from .logger import LOG
-from .config import config
-from .constant import SC
-from .user_constant import RC
-
-__all__ = ["LOG", "config", "SC", "RC"]
--- src/model/comment.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Comment:
-    from_user_name: str
-    comment_type: int
-    content: str
