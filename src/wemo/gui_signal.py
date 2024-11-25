42202829cf8130283366a13c114407ad6037538d
Jackylee <jieli.work@qq.com>
2024-11-25 14:02:30 +0800

    🚧 change signal

--- src/wemo/gui_signal.py
+++ src/wemo/gui_signal.py
@@ -5,16 +5,15 @@ from PySide6.QtCore import QObject, Signal, SignalInstance
 class GuiSignal(QObject):
     test_processing: SignalInstance = Signal(int)
 
-    sync_progress: SignalInstance = Signal(float)
-    update_progress: SignalInstance = Signal(float)
-    render_progress: SignalInstance = Signal(float)
+    sync_progress: SignalInstance = Signal(str)
+    update_progress: SignalInstance = Signal(str)
+    render_progress: SignalInstance = Signal(str)
 
     contacts_update_signal: SignalInstance = Signal(dict)
     latest_feed_update_signal: SignalInstance = Signal(str)
 
     out_dir_signal: SignalInstance = Signal(str)
 
-    info_update_signal: SignalInstance = Signal(str, str)
     logging_signal: SignalInstance = Signal(str, logging.LogRecord)
 
     def __init__(self):
